4b03d80f97b57baa913c7acdf7b20348ecd9e51e
Chunt0 <chunt.mail@gmail.com>
2022-04-09 18:23:01 -0700

    Added selection menu and updated driver

--- CS162-Assignment2/network.py
+++ CS162-Assignment2/network.py
@@ -1,20 +1,19 @@
-# [*] CHRISTOPHER HUNT
-# [*] network.py
+# CHRISTOPHER HUNT
+# network.py
 
-import random
 import numpy as np
-from MNIST_crit_funcs import sigmoid, relu, softmax
+from MNIST_crit_funcs import sigmoid, relu, softmax, fileInput
 
 
 class Network:
-    def __init__(self, sizes):
+    def __init__(self,sizes):
         """Initializes the random weights and biases matrices. Also holds the sizes 
         and number of layers variables. Tricky to build weights and biases"""
 
-        self.num_layers = len(sizes)
         self.sizes = sizes
-        self.weights = [np.random.randn(x,y) for x,y in zip(sizes[1:], sizes[:-1])]  # (from index 1 until the end, from index 0 to one before the end)
-        self.biases = [np.random.randn(y,1) for y in sizes[1:]] # (from index 1 until the end, always 1)
+        self.num_layers = len(self.sizes)
+        self.weights = [np.random.randn(x,y) for x,y in zip(self.sizes[1:], self.sizes[:-1])]  # (from index 1 until the end, from index 0 to one before the end)
+        self.biases = [np.random.randn(y,1) for y in self.sizes[1:]] # (from index 1 until the end, always 1)
 
     def feedForwardRelu(self, a):
         """Return the output of the network if "a" is input using ReLU and softmax as 
@@ -44,5 +43,32 @@ class Network:
         if test_data: n_test = len(test_data)
         pass
 
+#[*] Here is my selection menu where I utilize try and except clauses and access my file reading function. 
     def selectionMenu(self):
-        pass
+        on = True
+        try:
+            file = input("Provide path to Test or Train csv file: ")
+            image, label = fileInput(file, 10000)
+            while(on):
+                          
+                print("What would you like to do?\n1. Feed Forward with ReLU\n2.Feed Forward with Sigmoid\n3. Exit\n")
+                selection = int(input(":: "))
+                if(selection == 1):
+                    print(self.feedForwardRelu(image))
+                elif(selection == 2):
+                    print(self.feedForwardSigmoid(image))
+                elif(selection == 3):
+                    print("\nGood Bye!")
+                    on = False
+                else:
+                    print("\nWhat ever you entered didn't make sense. Try again.\n")
+
+        except ValueError:
+            print("\n####VALUEERROR####\nYOU MUST HAVE ENTERED A CHARACTER STRING WHEN YOU SHOULD HAVE ENTERED AN INTEGER...")
+        except FileNotFoundError:
+            print("\n####FILENOTFOUNDERROR####\nTHE FILE PATH YOU ENTERED MUST HAVE BEEN WRONG...")
+        except:
+            print("\n####UNKNOWNERROR####\nI DON'T KNOW WHAT YOU DID WRONG...")
+
+
+
