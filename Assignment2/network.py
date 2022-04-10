8fb6de28ed1bb00dcea1f099f978d2f3e82f67f4
Chunt0 <chunt.mail@gmail.com>
2022-04-10 15:00:42 -0700

    Updated

--- Assignment2/network.py
+++ Assignment2/network.py
@@ -8,8 +8,10 @@ from MNIST_crit_funcs import sigmoid, relu, softmax, fileInput
 
 class Network:
     def __init__(self,sizes):
-        """Initializes the random weights and biases matrices. Also holds the sizes 
-        and number of layers variables. Tricky to build weights and biases"""
+        """
+        Initializes the random weights and biases matrices. Also holds the sizes 
+        and number of layers variables. Tricky to build weights and biases
+        """
         self.sizes = sizes
         self.num_layers = len(self.sizes)
         self.weights = [np.random.randn(x,y) for x,y in zip(self.sizes[1:], self.sizes[:-1])]  # (from index 1 until the end, from index 0 to one before the end)
@@ -22,9 +24,11 @@ class Network:
         self.eta = None
 
     def feedForwardRelu(self, a):
-        """Return the output of the network if "a" is input using ReLU and softmax as 
+        """
+        Return the output of the network if "a" is input using ReLU and softmax as 
         activation functions - ReLU on all layers except the final which uses a softmax
-        function to return values between 0 - 1 """
+        function to return values between 0 - 1 
+        """
         # Both the weight and bias matrices have the same amount of layers (in this instance it is two, in each layer the matrices have the same rows but different collumn amount)
         last = len(self.biases)-1
         count = 0
@@ -37,8 +41,10 @@ class Network:
         return a
 
     def feedForwardSigmoid(self, a):
-        """Returns the output of the network if "a" is the input using the Sigmoid
-        function as the activation function on all layers"""
+        """
+        Returns the output of the network if "a" is the input using the Sigmoid
+        function as the activation function on all layers
+        """
         for b, w in zip(self.biases, self.weights):
             a = sigmoid(np.dot(w,a)+b)
         return a
@@ -46,7 +52,6 @@ class Network:
     def makeMiniBatch(self):
         """Function to generate mini batches to be used in Stochastic Gradient Descent"""
         # Create List of random int from 0 to self.input_size to randomize input file
-        
         shuffled_index = (list(range(0,self.input_size)))
         random.shuffle(shuffled_index)
         data = []
@@ -60,8 +65,10 @@ class Network:
         return mini_batches
 
     def stochasticGradientDescent(self, training_data, epochs, mini_batch_size, eta, test_data=None):
-        """Finding Global Minima -> in this case minimizing the cost function. Stochastic Gradient Descent is designed such that
-        we can better avoid getting caught in local minima to find the global minima."""
+        """
+        Finding Global Minima -> in this case minimizing the cost function. Stochastic Gradient Descent is designed such that
+        we can better avoid getting caught in local minima to find the global minima.
+        """
         pass
 
 ##########################################################################################################
@@ -71,7 +78,6 @@ class Network:
         """Selection Menu. This function allows the Network class object to have a User Interface"""
         on = True
         try:
-
             file = input("Provide path to Test or Train csv file: ")
             test_or_train = abs(int(input("Test or Train?\n1. Test\n2. Train\n:: ")))
             if (test_or_train == 1):
@@ -80,7 +86,6 @@ class Network:
                 self.input_size = 60000
             self.image, self.label = fileInput(file, self.input_size)
             while(on):
-
                 print("What would you like to do?\n1. Feed Forward with ReLU\n2. Feed Forward with Sigmoid\n3. Developers Options\n4. Exit.\n")
                 selection = int(input(":: "))
                 if(selection == 1):
@@ -94,7 +99,6 @@ class Network:
                 elif(selection == 4):
                     print("\nGood Bye!")
                     on = False
-
                 else:
                     print("\nWhat ever you entered didn't make sense. Try again.\n")
     
@@ -102,8 +106,3 @@ class Network:
             print("\n####VALUEERROR####\nYOU MUST HAVE ENTERED A CHARACTER STRING WHEN YOU SHOULD HAVE ENTERED AN INTEGER...")
         except FileNotFoundError:
             print("\n####FILENOTFOUNDERROR####\nTHE FILE PATH YOU ENTERED MUST HAVE BEEN WRONG...")
-        except:
-            print("\n####UNKNOWNERROR####\nI DON'T KNOW WHAT YOU DID WRONG...")
-
-
-
