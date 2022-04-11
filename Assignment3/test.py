2818856f50089fd465ec1347df9ed8329aa0a71a
Chunt0 <chunt.mail@gmail.com>
2022-04-11 11:46:02 -0700

    Updated 2022-04-11

--- Assignment3/test.py
+++ Assignment3/test.py
@@ -1,7 +1,25 @@
+from optparse import OptionGroup
 import turtle
-ruby = turtle.Turtle()
-ruby.shape("turtle")
-ruby.color("blue")
+
+windowsize = 1000
+
+
 window = turtle.Screen()
+window.setup(width = windowsize, height = windowsize)
+
+colors = ["purple","red","orange","yellow","green"]
+y_poses = [60, 30, 0, -30, -60]
+opponents = []
+
+for color, y_pos in zip(colors, y_poses):
+    new_turtle = turtle.Turtle("turtle")
+    new_turtle.penup()
+    new_turtle.color(color)
+    new_turtle.goto(x=-390, y = y_pos)
+    opponents.append(new_turtle)
+
+# make turtles move
+
+# win condition
 
 window.exitonclick()
