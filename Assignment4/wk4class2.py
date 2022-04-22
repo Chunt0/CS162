0b3ba338c3a20873af00a136cfc5aefc9cd07854
Chunt0 <chunt.mail@gmail.com>
2022-04-21 19:45:23 -0700

    Updated 2022-04-21

--- Assignment4/wk4class2.py
+++ Assignment4/wk4class2.py
@@ -3,39 +3,6 @@ Working with tkinter
 """
 
 import tkinter as tk
-
-# def mi_to_km():
-#     miles = float(my_input.get())
-#     miles_label.config(text=f"Miles: {miles}")
-#     kilometers = miles * 1.609344
-#     kilometers_label.config(text=f"Kilometers: {kilometers}")
-
-# root = tk.Tk()
-# root.title(":D")
-# root.geometry("400x125")
-# root.config(padx=20, pady=20)
-
-# welcome_label = tk.Label(root, text="Miles to Kilometer Converter!")
-# welcome_label.grid(column=0, row=0)
-
-# name_label= tk.Label(root, text="Enter Miles: ")
-# name_label.grid(column=0, row=1)
-
-# my_input = tk.Entry(root)
-# my_input.grid(column=1, row=1)
-
-# my_button = tk.Button(root, text="Click", command=mi_to_km)
-# my_button.grid(column=0, row=2)
-
-# miles_label = tk.Label(root, text="")
-# miles_label.grid(column=1, row=2)
-
-# kilometers_label = tk.Label(root, text="")
-# kilometers_label.grid(column=1, row=3)
-
-# root.mainloop()
-
-import tkinter as tk
 from turtle import width
 
 def count_down(text):
