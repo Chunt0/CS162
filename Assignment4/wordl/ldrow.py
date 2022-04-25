523e087258a2db4c898498c21b55e10b310bc26b
Chunt0 <chunt.mail@gmail.com>
2022-04-24 18:29:27 -0700

    Gitterdone

--- Assignment4/wordl/ldrow.py
+++ Assignment4/wordl/ldrow.py
@@ -43,6 +43,146 @@ W_label = tk.Label(text="W", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold")
 W_label.grid(column=5,row=0)
 #################################################################################
 
+################################### ROW 1 #######################################
+canvas = tk.Canvas(width=50, height=50, bg=PURPLE)
+canvas.grid(column=1, row=3)
+R3_C1_label = tk.Label(text="", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold"))
+R3_C1_label.grid(column=1,row=3)
+
+canvas = tk.Canvas(width=50, height=50, bg=PURPLE)
+canvas.grid(column=2, row=3)
+D_label = tk.Label(text="", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold"))
+D_label.grid(column=2,row=3)
+
+canvas = tk.Canvas(width=50, height=50, bg=PURPLE)
+canvas.grid(column=3, row=3)
+R_label = tk.Label(text="", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold"))
+R_label.grid(column=3,row=3)
+
+canvas = tk.Canvas(width=50, height=50, bg=PURPLE)
+canvas.grid(column=4, row=3)
+O_label = tk.Label(text="", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold"))
+O_label.grid(column=4,row=3)
+
+canvas = tk.Canvas(width=50, height=50, bg=PURPLE)
+canvas.grid(column=5, row=3)
+W_label = tk.Label(text="", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold"))
+W_label.grid(column=5,row=3)
+#################################################################################
+
+################################### ROW 2 #######################################
+canvas = tk.Canvas(width=50, height=50, bg=PURPLE)
+canvas.grid(column=1, row=4)
+L_label = tk.Label(text="", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold"))
+L_label.grid(column=1,row=4)
+
+canvas = tk.Canvas(width=50, height=50, bg=PURPLE)
+canvas.grid(column=2, row=4)
+D_label = tk.Label(text="", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold"))
+D_label.grid(column=2,row=4)
+
+canvas = tk.Canvas(width=50, height=50, bg=PURPLE)
+canvas.grid(column=3, row=4)
+R_label = tk.Label(text="", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold"))
+R_label.grid(column=3,row=4)
+
+canvas = tk.Canvas(width=50, height=50, bg=PURPLE)
+canvas.grid(column=4, row=4)
+O_label = tk.Label(text="", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold"))
+O_label.grid(column=4,row=4)
+
+canvas = tk.Canvas(width=50, height=50, bg=PURPLE)
+canvas.grid(column=5, row=4)
+W_label = tk.Label(text="", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold"))
+W_label.grid(column=5,row=4)
+#################################################################################
+
+################################## ROW 3 #######################################
+canvas = tk.Canvas(width=50, height=50, bg=PURPLE)
+canvas.grid(column=1, row=5)
+L_label = tk.Label(text="", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold"))
+L_label.grid(column=1,row=5)
+
+canvas = tk.Canvas(width=50, height=50, bg=PURPLE)
+canvas.grid(column=2, row=5)
+D_label = tk.Label(text="", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold"))
+D_label.grid(column=2,row=5)
+
+canvas = tk.Canvas(width=50, height=50, bg=PURPLE)
+canvas.grid(column=3, row=5)
+R_label = tk.Label(text="", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold"))
+R_label.grid(column=3,row=5)
+
+canvas = tk.Canvas(width=50, height=50, bg=PURPLE)
+canvas.grid(column=4, row=5)
+O_label = tk.Label(text="", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold"))
+O_label.grid(column=4,row=5)
+
+canvas = tk.Canvas(width=50, height=50, bg=PURPLE)
+canvas.grid(column=5, row=5)
+W_label = tk.Label(text="", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold"))
+W_label.grid(column=5,row=5)
+#################################################################################
+
+################################### ROW 4 #######################################
+canvas = tk.Canvas(width=50, height=50, bg=PURPLE)
+canvas.grid(column=1, row=6)
+L_label = tk.Label(text="", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold"))
+L_label.grid(column=1,row=6)
+
+canvas = tk.Canvas(width=50, height=50, bg=PURPLE)
+canvas.grid(column=2, row=6)
+D_label = tk.Label(text="", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold"))
+D_label.grid(column=2,row=6)
+
+canvas = tk.Canvas(width=50, height=50, bg=PURPLE)
+canvas.grid(column=3, row=6)
+R_label = tk.Label(text="", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold"))
+R_label.grid(column=3,row=6)
+
+canvas = tk.Canvas(width=50, height=50, bg=PURPLE)
+canvas.grid(column=4, row=6)
+O_label = tk.Label(text="", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold"))
+O_label.grid(column=4,row=6)
+
+canvas = tk.Canvas(width=50, height=50, bg=PURPLE)
+canvas.grid(column=5, row=6)
+W_label = tk.Label(text="", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold"))
+W_label.grid(column=5,row=6)
+#################################################################################
+
+################################### ROW 5 #######################################
+canvas = tk.Canvas(width=50, height=50, bg=PURPLE)
+canvas.grid(column=1, row=7)
+L_label = tk.Label(text="", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold"))
+L_label.grid(column=1,row=7)
+
+canvas = tk.Canvas(width=50, height=50, bg=PURPLE)
+canvas.grid(column=2, row=7)
+D_label = tk.Label(text="", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold"))
+D_label.grid(column=2,row=7)
+
+canvas = tk.Canvas(width=50, height=50, bg=PURPLE)
+canvas.grid(column=3, row=7)
+R_label = tk.Label(text="", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold"))
+R_label.grid(column=3,row=7)
+
+canvas = tk.Canvas(width=50, height=50, bg=PURPLE)
+canvas.grid(column=4, row=7)
+O_label = tk.Label(text="", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold"))
+O_label.grid(column=4,row=7)
+
+canvas = tk.Canvas(width=50, height=50, bg=PURPLE)
+canvas.grid(column=5, row=7)
+W_label = tk.Label(text="", bg=GREEN, fg=PURPLE, font=("Helvetica", 48, "bold"))
+W_label.grid(column=5,row=7)
+#################################################################################
+
+
+
+
+
+
 
 
 # Button
