1ceacf13faecd8d8694ed58e86e1bef8c76b14f0
Chunt0 <chunt.mail@gmail.com>
2022-04-29 11:32:10 -0700

    Gitterdone

--- Assignment4/converter.py
+++ Assignment4/converter.py
@@ -14,14 +14,26 @@ class Converter(tk.Tk):
 class MilesToKm(tk.Frame):
     def __init__(self, container):
         super().__init__(container)
+
+        self.input = tk.StringVar()
+
+        self.mile_to_km_label = tk.Label(self, text="Miles to Km")
+        self.user_input = tk.Entry(self, textvariable=self.input) 
+        self.convert_button = tk.Button(self, text="convert", command=self.calculate)
+        self.result_label = tk.Label(self)
         
-        mile_to_km_label = tk.Label(self, text="Miles to Km")
-        user_input = tk.Entry(self)
-        convert_button = tk.Button(self, text="convert")
-        
-        mile_to_km_label.grid(column=0, row=0)
-        user_input.grid(column=1, row=0)
-        convert_button.grid(column=0, row=1)
+        self.mile_to_km_label.grid(column=0, row=0)
+        self.user_input.grid(column=1, row=0)
+        self.convert_button.grid(column=0, row=1)
+        self.result_label.grid(column=1, row=1)
+
+    def calculate(self):
+        try:
+            mi = self.user_input.get()
+            km = 1.60934 * float(mi)
+            self.result_label.config(text=f"{mi} mi -> {km} km")
+        except:
+            pass
 
 converter = Converter()
 
