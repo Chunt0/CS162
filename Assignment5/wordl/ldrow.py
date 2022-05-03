e94fee560082e0488ed5d935e795e1128b016b5e
Chunt0 <chunt.mail@gmail.com>
2022-05-02 18:23:48 -0700

    Gitterdone

--- Assignment5/wordl/ldrow.py
+++ Assignment5/wordl/ldrow.py
@@ -6,100 +6,113 @@ import random
 import tkinter as tk
 from ldrow_funcs import get_wordlist, match_word
 
-MAX_GUESSES = 6
-GUESS = 0
 
 ################################# COLORS ########################################
-BLACK = "black"
-WHITE = "white"
-YELLOW = "#FFCA03"
-GREEN = "#62B73E"
-GREY = "#5B5B5B"
-#################################################################################
+################################################################################
 
 ############################## WINDOW CLASS ####################################
-class Root(tk.Tk):
+class Ldrow(tk.Tk):
     """Root window that UI exists in."""
     def __init__(self):
         super().__init__()
-        self.window =tk.Tk()
+        self.window = tk.Tk()
         self.window.title("ldrow")
-        self.window.config(padx=50, pady=50, bg=BLACK)
+        self.window.config(padx=50, pady=50, bg="black")
+        self.row1 = GuessRow(1)
+        self.row2 = GuessRow(2)
+        self.row3 = GuessRow(3)
+        self.row4 = GuessRow(4)
+        self.row5 = GuessRow(5)
+        self.row6 = GuessRow(6)
+        self.title_row = Title()
 
-################################################################################
+        # Rows are updated each round a player guesses
+        self.guess_matrix = [
+                self.row1.row,
+                self.row2.row,
+                self.row3.row,
+                self.row4.row,
+                self.row5.row,
+                self.row6.row
+                ]
 
-############################# LDROW FUNCTIONS ##################################
+        # Untouchables
+        self._wordlist = get_wordlist("./wordlist.txt")
+        self._word = random.choice(self._wordlist)
+        self._GUESS = 0
+        self._MAX_GUESS = 6
+        self._YELLOW = "#FFCA03"
+        self._GREEN = "#62B73E"
+        self._GREY = "#5B5B5B"
 
-def reset_game():
-    global GUESS
-    global word
-    for guess in range(0,6):
-        for index in range(0,5):
-            guess_matrix[guess][index].config(text="", bg=BLACK)
-    word = random.choice(wordlist)
-    GUESS = 0
-    print(word)
+    def reset_game(self):
+        for guess in range(0,6):
+            for index in range(0,5):
+                self.guess_matrix[guess][index].config(text="", bg="black")
+        self._word = random.choice(self._wordlist)
+        self._GUESS = 0
+        print(self._word)
 
-def make_guess():
-    global GUESS 
-    global MAX_GUESSES
-    guessed_word = guess_entry.get()
-    if GUESS < MAX_GUESSES and len(guessed_word) == 5 and wordlist.count(guessed_word) > 0:
-        solution = match_word(word, guessed_word)
-        for index, answer in enumerate(solution):
-            if answer[1] == 0:
-                guess_matrix[GUESS][index].config(text=answer[0], fg=WHITE, bg=GREY)
-            elif answer[1] == 1:
-                guess_matrix[GUESS][index].config(text=answer[0], fg=WHITE, bg=GREEN)
-            elif answer[1] == 2:
-                guess_matrix[GUESS][index].config(text=answer[0], fg=WHITE, bg=YELLOW)
-        GUESS += 1
-    elif GUESS == MAX_GUESSES:
-        GUESS = 0
-    else:
-        pass
+    def make_guess(self):
+        guessed_word = guess_entry.get()
+        if self._GUESS < self._MAX_GUESS and len(guessed_word) == 5 and self._wordlist.count(guessed_word) > 0:
+            solution = match_word(self._word, guessed_word)
+            for index, answer in enumerate(solution):
+                if answer[1] == 0:
+                    self.guess_matrix[self._GUESS][index].config(text=answer[0], fg="white", bg=self._GREY)
+                elif answer[1] == 1:
+                    self.guess_matrix[self._GUESS][index].config(text=answer[0], fg="white", bg=self._GREEN)
+                elif answer[1] == 2:
+                    self.guess_matrix[self._GUESS][index].config(text=answer[0], fg="white", bg=self._YELLOW)
+            self._GUESS += 1
+        elif self._GUESS == self._MAX_GUESS:
+            self._GUESS = 0
+        else:
+            pass
 
 ################################################################################
 
 ################################### TITLE CLASS #######################################
 class Title(tk.Label):
+    """Creates the title label"""
     def __init__(self):
         super().__init__()
 
-        self.L_label = tk.Label(text="L", bg=BLACK, fg=WHITE, font=("Helvetica", 48, "bold"))
+        self.L_label = tk.Label(text="L", bg="black", fg="white", font=("Helvetica", 48, "bold"))
         self.L_label.grid(column=1,row=0)
 
-        self.D_label = tk.Label(text="D", bg=BLACK, fg=WHITE, font=("Helvetica", 48, "bold"))
+        self.D_label = tk.Label(text="D", bg="black", fg="white", font=("Helvetica", 48, "bold"))
         self.D_label.grid(column=2,row=0)
 
-        self.R_label = tk.Label(text="R", bg=BLACK, fg=WHITE, font=("Helvetica", 48, "bold"))
+        self.R_label = tk.Label(text="R", bg="black", fg="white", font=("Helvetica", 48, "bold"))
         self.R_label.grid(column=3,row=0)
 
-        self.O_label = tk.Label(text="O", bg=BLACK, fg=WHITE, font=("Helvetica", 48, "bold"))
+        self.O_label = tk.Label(text="O", bg="black", fg="white", font=("Helvetica", 48, "bold"))
         self.O_label.grid(column=4,row=0)
 
-        self.W_label = tk.Label(text="W", bg=BLACK, fg=WHITE, font=("Helvetica", 48, "bold"))
+        self.W_label = tk.Label(text="W", bg="black", fg="white", font=("Helvetica", 48, "bold"))
         self.W_label.grid(column=5,row=0)
 #################################################################################
 
 ################################ GUESS ROW CLASS ####################################
 class GuessRow(tk.Label):
-    def __init__(self):
+    """Generates labels of the players guess."""
+    def __init__(self,ROW):
         super().__init__()
-        self.C1_label = tk.Label(text="", bg=BLACK, fg=WHITE, font=("Helvetica", 48, "bold"))
-        self.C1_label.grid(column=1,row=3)
+        self.C1_label = tk.Label(text="", bg="black", fg="white", font=("Helvetica", 48, "bold"))
+        self.C1_label.grid(column=1,row=ROW)
 
-        self.C2_label = tk.Label(text="", bg=BLACK, fg=WHITE, font=("Helvetica", 48, "bold"))
-        self.C2_label.grid(column=2,row=3)
+        self.C2_label = tk.Label(text="", bg="black", fg="white", font=("Helvetica", 48, "bold"))
+        self.C2_label.grid(column=2,row=ROW)
 
-        self.C3_label = tk.Label(text="", bg=BLACK, fg=WHITE, font=("Helvetica", 48, "bold"))
-        self.C3_label.grid(column=3,row=3)
+        self.C3_label = tk.Label(text="", bg="black", fg="white", font=("Helvetica", 48, "bold"))
+        self.C3_label.grid(column=3,row=ROW)
 
-        self.C4_label = tk.Label(text="", bg=BLACK, fg=WHITE, font=("Helvetica", 48, "bold"))
-        self.C4_label.grid(column=4,row=3)
+        self.C4_label = tk.Label(text="", bg="black", fg="white", font=("Helvetica", 48, "bold"))
+        self.C4_label.grid(column=4,row=ROW)
 
-        self.C5_label = tk.Label(text="", bg=BLACK, fg=WHITE, font=("Helvetica", 48, "bold"))
-        self.C5_label.grid(column=5,row=3)
+        self.C5_label = tk.Label(text="", bg="black", fg="white", font=("Helvetica", 48, "bold"))
+        self.C5_label.grid(column=5,row=ROW)
 
         self.row = [self.C1_label, self.C2_label, self.C3_label, self.C4_label, self.C5_label]
 #################################################################################
@@ -119,11 +132,7 @@ guess_button.grid(column=2,row=1)
 #################################################################################
 
 #################################################################################
-path_to_wordlist = "./wordlist.txt"
-wordlist = get_wordlist(path_to_wordlist)
-word = random.choice(wordlist)
-print(word)
 
-window = Root()
+window = Ldrow()
 
 window.mainloop()
