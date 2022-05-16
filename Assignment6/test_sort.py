0ae0acb49e3fcd9406bdd56c0106126e044ab3d3
Chunt0 <chunt.mail@gmail.com>
2022-05-16 10:28:25 -0700

    Gitterdone

--- Assignment6/test_sort.py
+++ Assignment6/test_sort.py
@@ -24,18 +24,15 @@ def test_bubble_sort():
     rand_list = bubble_sort(rand_list)
     for index in range(0, len(rand_list)-1):
         assert rand_list[index] <= rand_list[index+1]
-pass
 
 def test_insertion_sort():
     rand_list = np.random.randint(0,10,10)
     rand_list = insertion_sort(rand_list)
     for index in range(0, len(rand_list)-1):
         assert rand_list[index] <= rand_list[index+1]
-    pass
 
 def test_merge_sort():
     rand_list = np.random.randint(0,10,10)
     rand_list = merge_sort(rand_list)
     for index in range(0, len(rand_list)-1):
         assert rand_list[index] <= rand_list[index+1]
-    pass
