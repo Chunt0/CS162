b9bbbd49caaeb969f20ac73ba4d4f320011e4376
Chunt0 <chunt.mail@gmail.com>
2022-05-28 10:18:30 -0700

    Updated

--- Assignment7/animals.py
+++ Assignment7/animals.py
@@ -5,27 +5,38 @@
 ################################################################################
 
 class Animal():
+    """Generic Animal class"""
     def __init__(self):
+        """__init__"""
         self.HP = 100
         
     def eat(self):
+        """Animals gotta eat. Restores one energy point."""
         print("Ate. Restored one energy point.")
         return 1
 
     def rest(self):
+        """Animals gotta sleep. Restores one energy point."""
         print("Rested. Restored one energy point.")
         return 1
 
     def sound(self):
+        """
+        Animals make sounds. 
+        This is essentially here to demonstrate polymorphism.
+        """
         print("~~Animal sounds~~")
     
     def __string__(self):
+        """Prints stats."""
         print(f"HP: {self.HP}")
 
 ################################################################################
 
 class Bear(Animal):
+    """Child class of Animal"""
     def __init__(self):
+        """__init__"""
         super().__init__()
         self.speed = 1
         self.power = 3
@@ -34,6 +45,7 @@ class Bear(Animal):
         self.max_age = 16
 
     def run(self, p1loc, p2loc):
+        """Moves Bear closer to opponent."""
         run = 0
         if p1loc < p2loc:
             run = self.speed
@@ -43,9 +55,11 @@ class Bear(Animal):
         return run
 
     def attack(self):
+        """Causes damage to opponent."""
         return self.power
 
     def evade(self, p1loc, p2loc):
+        """Moves Bear farther from opponent."""
         evade = 0
         if p1loc < p2loc:
             evade = -self.speed
@@ -55,15 +69,19 @@ class Bear(Animal):
         return evade
 
     def sound(self):
+        """Bear makes a sound. Demonstrates polymorphism."""
         print("~Roaar~")
 
     def __string__(self):
+        """Prints Bear's stats."""
         print(f"HP: {self.HP}\nSpeed: {self.speed}\nPower: {self.power}\nDefense: {self.defense}\nEnergy: {self.energy}\nMax Age: {self.max_age}")
 
 ################################################################################
 
 class Eagle(Animal):
+    """Child class of Animal"""
     def __init__(self):
+        """__init__"""
         super().__init__()
         self.speed = 3
         self.power = 2
@@ -72,6 +90,7 @@ class Eagle(Animal):
         self.max_age = 20
 
     def fly(self, p1loc, p2loc):
+        """Moves Eagle closer to opponent."""
         fly = 0
         if p1loc < p2loc:
             fly = self.speed
@@ -81,9 +100,11 @@ class Eagle(Animal):
         return fly
 
     def attack(self):
+        """Causes damage to opponent."""
         return self.power
 
     def evade(self, p1loc, p2loc):
+        """Moves Eagle farther from opponent."""
         evade = 0
         if p1loc < p2loc:
             evade = -self.speed
@@ -93,6 +114,7 @@ class Eagle(Animal):
         return evade
 
     def sound(self):
+        """Eagle makes sound. Demonstrates polymorphism."""
         print("~Screee~")
     
     def __string__(self):
