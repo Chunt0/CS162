2ef3bd655bac89a7f6d16214632e4d4304e03b2a
Chunt0 <chunt.mail@gmail.com>
2022-05-28 11:15:29 -0700

    Updated

--- Assignment7/driver.py
+++ Assignment7/driver.py
@@ -8,6 +8,7 @@ from animals import Bear, Eagle, Salmon
 class Arena():
     """Class object that allows for two animals to battle."""
     def __init__(self, player):
-        self.player = player
-        self.comp = random.choice([Bear(), Eagle(), Salmon()])
+        self.animals = [Bear(), Eagle(), Salmon()]
+        self.player = random.choice(self.animals)
+        self.comp = random.choice(self.animals)
 
