fb5b26658829d9b2716e6fe5d5d9a63b906c6522
Chunt0 <chunt.mail@gmail.com>
2022-05-28 17:20:51 -0700

    Updated

--- Assignment7/driver.py
+++ Assignment7/driver.py
@@ -14,22 +14,25 @@ class Arena():
         self.comp = random.choice([Bear(self.right), Eagle(self.right), Salmon(self.right)])
 
     def playerattack(self):
+        """Player attacks comp"""
         if abs((self.player.pos - self.comp.pos)) < self.player.reach:
-            damage = self.player.attack() * random.randint(1,5)
+            damage = (self.player.attack() * random.randint(1,5)) - self.comp.defense
             self.comp.damage(damage)
         else:
             print("Player is out of range!")
             self.player.checkEnergy()
 
     def compAttack(self):
+        """Comp attacks player"""
         if abs((self.comp.pos - self.player.pos)) < self.comp.reach:
-            damage = self.comp.attack() * random.randint(1,5)
+            damage = (self.comp.attack() * random.randint(1,5)) - self.player.defense
             self.player.damage(damage)
         else:
             print("Comp is out of range!")
             self.comp.checkEnergy()
 
     def playerMove(self):
+        """Player moves toward comp"""
         move = self.player.move(self.comp.pos)
         if (self.player.pos + move) < self.left:
             self.player.pos = self.left
@@ -39,6 +42,7 @@ class Arena():
             self.player.pos += move
 
     def compMove(self):
+        """Comp moves toward player"""
         move = self.comp.move(self.player.pos)
         if (self.comp.pos + move) < self.left:
             self.comp.pos = self.left
@@ -48,6 +52,7 @@ class Arena():
             self.comp.pos += move
 
     def playerEvade(self):
+        """Player moves away from comp"""
         move = self.player.evade(self.comp.pos)
         if (self.player.pos + move) < self.left:
             self.player.pos = self.left
@@ -57,6 +62,7 @@ class Arena():
             self.player.pos += move
 
     def compEvade(self):
+        """Comp moves away from player"""
         move = self.comp.evade(self.player.pos)
         if (self.comp.pos + move) < self.left:
             self.comp.pos = self.left
@@ -66,16 +72,20 @@ class Arena():
             self.comp.pos += move
 
     def playerEat(self):
-        pass
+        """Player restores an energy point"""
+        self.player.eat()
 
     def compEat(self):
-        pass
+        """Comp restores an energy point"""
+        self.comp.eat()
 
     def playerSound(self):
-        pass
+        """Player makes a sound"""
+        self.player.sound()
 
     def compSound(self):
-        pass
+        """Comp makes a sound"""
+        self.comp.sound()
 
     def menu(self):
         """Menu function, used in driver to play game."""
