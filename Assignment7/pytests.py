4cc22ca81d50a40052705a15972127358f757f66
Chunt0 <chunt.mail@gmail.com>
2022-05-29 11:13:09 -0700

    Updated

--- Assignment7/pytests.py
+++ Assignment7/pytests.py
@@ -3,25 +3,29 @@
 # pytests.py
 
 from animals import Animal, Bear, Eagle, Salmon
-from arena import Arena
-from exceptions import AnimalDies, AnimalError, AnimalExhaustion
 import random
 
 def testEnergyDepletion():
-    animal = Animal()
-
-    energy = animal.energy
-    animal.sound()
-    assert energy > animal.energy # Make sure energy is going down when an action occurs
+    """Runs 100 tests to make sure animal.energy is going down when sound() is called."""
+    for _ in range(100):
+        animal = random.choice([Bear(random.randint(-10,10)), Eagle(random.randint(-10,10)), Salmon(random.randint(-10,10))])
+        energy = animal.energy
+        random.choice([animal.sound(), animal.move(0), animal.evade(0), animal.attack()])
+        assert energy > animal.energy # Make sure energy is going down when an action occurs
 
 def testHealthDepletion():
-    animal = Animal()
-
-    health = animal.HP
-    animal.damage(5)
-    assert health > animal.HP # Make sure health went down after damage function
+    """Runs 100 tests to make sure animal.HP is decreasing when damage() is called"""
+    for _ in range(100):
+        animal = random.choice([Bear(random.randint(-10,10)), Eagle(random.randint(-10,10)), Salmon(random.randint(-10,10))])
+        health = animal.HP
+        animal.damage(5)
+        assert health > animal.HP # Make sure health went down after damage function
 
 def testMoveFunction():
+    """
+    Runs 100 tests to make sure move() works properly - animal.pos is adjusted
+    in the correct direction: positive, negative, or zero if they are equal.
+    """
     for _ in range(100):
         animal1 = random.choice([Bear(random.randint(-10,10)), Eagle(random.randint(-10,10)), Salmon(random.randint(-10,10))])
         animal2 = random.choice([Bear(random.randint(-10,10)), Eagle(random.randint(-10,10)), Salmon(random.randint(-10,10))])
